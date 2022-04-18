bc1bd6862fb6e65f039799a0024caa1688566285
HunterWarburton <warburtonhunter@gmail.com>
2022-04-18 12:26:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ToDo List
+
+
+## DESCRIPTION
+This project is an example of react code using states to control a list of items.
+
+
+## Features/Learning Purposes
+This project demonstrates the usage of React, transpiling with Babel, running an Http-Server to load the webpage, and Styling a web app. During the creation of this app the code was also refactored from a simpler single page of code to being split into multiple components.
+
+## HOW TO USE
+This ToDo list starts with 4 elements coded in. New elements can be added by entering them into the input form box at the bottom of the list. Clicking on any of the items in the list removes them.
+
+
+## ROADMAP of FUTURE IMPROVEMENTS
+Saving the date in the list for future use and using in new sessions is the most desired new feature
+
+
+## LICENSING
+
+MIT License
---
+++ index.js
@@ -0,0 +1,48 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'Learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'Make a friend',
+      isCompleted: false,
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'Build todo app',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+
+  //remove item in list
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
